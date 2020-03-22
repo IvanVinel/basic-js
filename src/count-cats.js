86316fcec07cc98b3ddb90e538055b7d6e329585
IvanVinel <bjohnny051@gmail.com>
2020-03-22 23:48:42 +0300

    feat: implement count cats and dream team functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix.length === 0) return 0;
+  return matrix.reduce((acc, i) => {
+    acc += i.reduce((res, item) => {
+      if(item === '^^') return ++res;
+      return res;
+    },0)
+    return acc;
+  }, 0)
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array) || members.length === 0) return false;
+  const result = members.reduce((acc, item) => {
+    if (typeof item === 'string') {
+      return acc.concat(item.trim().charAt(0));
+    }
+
+    return acc;
+  }, [])
+  return result.filter(i => i.match(/[A-Za-z]/)).map(i => i.toUpperCase()).sort().join('');
 };
